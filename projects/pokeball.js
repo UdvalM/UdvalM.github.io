055a460ec945d3375424f940345f87ddae0d4211
Udval <seikolynx@gmail.com>
2025-04-30 11:34:59 -0400

    yeah

--- projects/pokeball.js
+++ projects/pokeball.js
@@ -5,21 +5,60 @@ const ctx = canvas.getContext("2d");
 ctx.fillStyle = "purple";
 ctx.fillRect(0,0,400,400);
 
-ctx.fillStyle = "white";
-ctx.fillRect(0,300,400,100);
+//pokeball shape stuff
 
-//pokeball
-function circle(x,y,r){
+function halfCircle(x,y,radius, isBottom) {
     ctx.beginPath();
-    ctx.arc(x,y,r,0,1*Math.PI);
+    if (isBottom) {
+        ctx.arc(x,y,radius,Math.PI, 0, false);
+    } else {
+        ctx.arc(x,y,radius, 0, Math.PI, false);
+    }
+    ctx.closePath();
     ctx.fill();
 }
 
-function ball(x,y){
-    circle(x,y,100); //upper
-    ctx.fillStyle="red"
-    circle(-x,-y,100); //lower
-    ctx.fillStyle="white"
+function circle(x,y,radius) {
+    ctx.beginPath();
+    ctx.arc(x,y,radius,0, 2*Math.PI);
+    ctx.closePath();
+    ctx.fill();
 }
 
-ball(100,100);
+//drawing the actual thing
+
+function ball(x,y){
+    ctx.fillStyle = "red";
+    halfCircle(x,y,100, true); //upper
+    ctx.fillStyle = "white";
+    halfCircle(x,y,100, false); //lower
+}
+
+function button(x,y) {
+    ctx.fillStyle = "white";
+    circle(x,y,30);
+}
+
+function lilBorder(x,y,radius, borderColor = "black", borderWidth = 7) {
+    ctx.strokeStyle = borderColor;
+    ctx.lineWidth = borderWidth;
+    ctx.beginPath();
+    ctx.arc(x,y,radius,0,2*Math.PI);
+    ctx.closePath();
+    ctx.stroke();
+}
+
+function lilLine(x1,y1,x2,y2, color = "black", width = 7) {
+    ctx.strokeStyle = color;
+    ctx.lineWidth = width;
+    ctx.beginPath();
+    ctx.moveTo(x1,y1);
+    ctx.lineTo(x2,y2);
+    ctx.stroke();
+}
+
+ball(200,200);
+lilLine(100,200,300,200);
+button(200,200);
+lilBorder(200,200,100);
+lilBorder(200,200,30);
